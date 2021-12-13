5d40d96aff793bbe83085a402ef80617322baeee
Yahya Bekir Canevi <152120181030@ogrenci.ogu.edu.tr>
2021-12-14 00:09:56 +0300

    ActivityEvaluation

--- flutter/lib/screens/ActivityForm/activity_evaluation.dart
+++ flutter/lib/screens/ActivityForm/activity_evaluation.dart
@@ -1,53 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
+import 'package:vtys_kalite/controller/activity_controller.dart';
+import 'package:vtys_kalite/controller/activity_evaluation_controller.dart';
+import 'package:vtys_kalite/controller/user_controller.dart';
+import 'package:vtys_kalite/models/activity.dart';
+import 'package:vtys_kalite/models/user.dart';
 import 'package:vtys_kalite/utilities/constans.dart';
 
 class ActivityEvaluationPage extends StatefulWidget {
   static String routeName = '/ActivityEvaluationPage';
 
-  const ActivityEvaluationPage({Key? key}) : super(key: key);
+  ActivityEvaluationPage({Key? key}) : super(key: key);
+  final TextEditingController _evaluationController = TextEditingController();
 
   @override
   _ActivityEvaluationPageState createState() => _ActivityEvaluationPageState();
 }
 
 class _ActivityEvaluationPageState extends State<ActivityEvaluationPage> {
+  late int userId, activityId;
+  final UserController userController = Get.put(UserController());
+  final ActivityController activityController = Get.put(ActivityController());
+  final ActivityEvaluationController activityEvaluationController = Get.put(ActivityEvaluationController());
+  late Activity activity;
+  late User user;
+
   @override
   Widget build(BuildContext context) {
+    var arguments = ModalRoute.of(context)!.settings.arguments as Map;
+    userId = arguments['userId'];
+    activityId = arguments['activityId'];
+    print("Activity ID : $activityId");
+    activity = activityController.activityList[activityId];
+    user = userController.userList[userId];
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Activity's Name"),
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    buildNameOfOrganizator(),
-                    buildDateOfActivity(),
-                  ],
-                ),
-                SizedBox(height: 50),
-                buildEvaluation(),
-                SizedBox(height: 50),
-                Column(
-                  children: [
-                    buildUsername(),
-                    buildButtonSave(),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        appBar: AppBar(
+          title: Obx(() {
+            return Text(activityController.isLoading.value
+                ? "Activity's Name"
+                : activity.name);
+          }),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Obx(() {
+          return (activityController.isLoading.value
+              ? const CircularProgressIndicator()
+              : SafeArea(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              buildNameOfOrganizator(),
+                              buildDateOfActivity(),
+                            ],
+                          ),
+                          const SizedBox(height: 50),
+                          buildEvaluation(),
+                          const SizedBox(height: 50),
+                          Column(
+                            children: [
+                              buildUsername(),
+                              buildButtonSave(),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ));
+        }));
   }
 
   Container buildButtonSave() {
@@ -55,7 +84,12 @@ class _ActivityEvaluationPageState extends State<ActivityEvaluationPage> {
       width: 250,
       height: 40,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            activityEvaluationController.postActivityEvaluation(activity.id, user.id, widget._evaluationController.text);
+            Navigator.pop(context);
+          });
+        },
         style: ButtonStyle(
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
@@ -71,12 +105,14 @@ class _ActivityEvaluationPageState extends State<ActivityEvaluationPage> {
   Container buildUsername() {
     return Container(
       width: 250,
-      child: Text("Username", style: kLabelStyle),
+      child: Text("Username : ${user.name}",
+          style: kLabelStyle),
     );
   }
 
   TextFormField buildEvaluation() {
     return TextFormField(
+      controller: widget._evaluationController,
       maxLines: 15,
       decoration: InputDecoration(
         hintText: "Please, write your thoughts about the event here...",
@@ -90,12 +126,8 @@ class _ActivityEvaluationPageState extends State<ActivityEvaluationPage> {
   Container buildDateOfActivity() {
     return Container(
       width: 250,
-      child: Column(
-        children: [
-          Text("Date", style: kLabelStyle),
-          Text("Tarihi Salla şuan"),
-        ],
-      ),
+      child: Text("Date : ${dateFormat.parse(activity.datetime).toString()}",
+          style: kLabelStyle),
     );
   }
 
@@ -104,8 +136,8 @@ class _ActivityEvaluationPageState extends State<ActivityEvaluationPage> {
       width: 250,
       child: Column(
         children: [
-          Text("Name", style: kLabelStyle),
-          Text("Aktiviteyi Oluşturan Kişi"),
+          Text("Name : ${userController.userList[userId].name}",
+              style: kLabelStyle),
         ],
       ),
     );
--- flutter/lib/screens/ActivityForm/activity_form.dart
+++ flutter/lib/screens/ActivityForm/activity_form.dart
@@ -4,10 +4,13 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:vtys_kalite/controller/activity_controller.dart';
+import 'package:vtys_kalite/controller/activity_evaluation_controller.dart';
 import 'package:vtys_kalite/controller/user_controller.dart';
 import 'package:vtys_kalite/models/activity.dart';
+import 'package:vtys_kalite/models/activity_evaluation.dart';
 import 'package:vtys_kalite/screens/ActivityForm/activity_evaluation.dart';
 import 'package:vtys_kalite/screens/ActivityForm/new_activity.dart';
+import 'package:vtys_kalite/utilities/constans.dart';
 
 class ActivityFormPage extends StatefulWidget {
   static String routeName = '/ActivityFormPage';
@@ -19,22 +22,25 @@ class ActivityFormPage extends StatefulWidget {
 }
 
 class _ActivityFormPageState extends State<ActivityFormPage> {
-  static int universal = 0;
+  static int universalUserId = 0;
+  static int activityId = 0;
 
   //List<String> activityString = ["asd", "qwe", "tyu", "bnm"];
 
   late UserController userController;
   late ActivityController activityController;
+  late ActivityEvaluationController activityEvaluationController;
 
   @override
   Widget build(BuildContext context) {
     userController = Get.put(UserController());
     activityController = Get.put(ActivityController());
-    universal = ModalRoute.of(context)!.settings.arguments as int;
+    activityEvaluationController = Get.put(ActivityEvaluationController());
+    universalUserId = ModalRoute.of(context)!.settings.arguments as int;
     print("activityList Size ${activityController.activityList.length}");
     return Scaffold(
       appBar: AppBar(
-        title: Obx(() => Text(userController.userList[universal].name)),
+        title: Obx(() => Text(userController.userList[universalUserId].name)),
         centerTitle: true,
         actions: [
           IconButton(
@@ -55,7 +61,7 @@ class _ActivityFormPageState extends State<ActivityFormPage> {
                     itemCount: activityController.activityList.length,
                     itemBuilder: (context, index) {
                       return buildActivityCard(
-                          activityController.activityList[index]);
+                          activityController.activityList[index], index);
                     },
                   ),
                 ));
@@ -64,19 +70,48 @@ class _ActivityFormPageState extends State<ActivityFormPage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, NewActivityPage.routeName,
-              arguments: universal);
+              arguments: universalUserId);
         },
         child: const Icon(Icons.add),
       ),
     );
   }
 
-  Card buildActivityCard(Activity activity) {
+  Card buildActivityCard(Activity activity, int index) {
+    activityId = index;
+    for (ActivityEvaluation ae
+        in activityEvaluationController.activityEvaluationList) {
+      if (ae.userId == userController.userList[universalUserId].id &&
+          ae.activityId == activityController.activityList[activityId].id) {
+        return Card(
+          child: ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(activity.name),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Icon(Icons.check, color: kColorGreen),
+                    Text(" Answered", style: TextStyle(color: kColorGreen)),
+                  ],
+                ),
+              ],
+            ),
+            subtitle: Text(activity.organizator),
+          ),
+        );
+      }
+    }
     return Card(
       child: ListTile(
         onTap: () {
           setState(() {
-            Navigator.pushNamed(context, ActivityEvaluationPage.routeName, arguments: activity.id);
+            Navigator.pushNamed(context, ActivityEvaluationPage.routeName,
+                arguments: {
+                  'userId': universalUserId,
+                  'activityId': index
+                });
           });
         },
         title: Text(activity.name),
@@ -88,7 +123,7 @@ class _ActivityFormPageState extends State<ActivityFormPage> {
                   activity.name, activity.organizator);
             });
           },
-          icon: const Icon(Icons.delete),
+          icon: const Icon(Icons.delete, color: kColorRed),
         ),
       ),
     );
