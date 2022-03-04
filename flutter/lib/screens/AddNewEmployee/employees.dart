e1c09ac6bd89ded5b31b71a462b87895d3d5ae2b
Yahya Bekir Canevi <152120181030@ogrenci.ogu.edu.tr>
2022-03-04 18:16:40 +0300

    Bug Fix

--- flutter/lib/screens/AddNewEmployee/employees.dart
+++ flutter/lib/screens/AddNewEmployee/employees.dart
@@ -9,21 +9,8 @@ import 'package:vtys_kalite/screens/AddNewEmployee/components/employee_card.dart
 import 'package:vtys_kalite/screens/AdminPanel/admin_panel.dart';
 import 'package:vtys_kalite/utilities/controllers.dart';
 
-class Employees extends StatefulWidget {
+class Employees extends StatelessWidget {
   const Employees({Key? key}) : super(key: key);
-
-  @override
-  State<Employees> createState() => _EmployeesState();
-}
-
-class _EmployeesState extends State<Employees> {
-  @override
-  void initState() {
-    super.initState();
-    if (userController.isLoading.isFalse) {
-      userController.fetchUsers();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,66 +25,63 @@ class _EmployeesState extends State<Employees> {
                       const Expanded(flex: 1, child: Text("")),
                       Expanded(
                         flex: ResponsiveWidget.isSmallScreen(context) ? 50 : 8,
-                        child: Stack(
+                        child: Column(
                           children: [
-                            ListView.builder(
-                              itemCount: userController.userList.length,
-                              itemBuilder: (context, index) {
-                                return EmployeeCard(
-                                  user: userController.userList[index],
-                                );
-                              },
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: userController.userList.length,
+                                itemBuilder: (context, index) {
+                                  return EmployeeCard(
+                                    user: userController.userList[index],
+                                  );
+                                },
+                              ),
                             ),
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Row(
-                                  children: [
+                            Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5),
+                                      child: CustomButton(
+                                        width: double.infinity,
+                                        title: 'Yeni Personel',
+                                        leftIcon: Icons.person_add,
+                                        pressAction: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (context) => Dialog(
+                                              child: AddNewEmployee(
+                                                user: null,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  if (user.title == DepartmentsEnum.management)
                                     Expanded(
                                       child: Padding(
                                         padding: const EdgeInsets.symmetric(
                                             horizontal: 5),
                                         child: CustomButton(
-                                          width: double.infinity,
-                                          title: 'Yeni Personel',
-                                          leftIcon: Icons.person_add,
+                                          title: 'Admin Panel',
+                                          leftIcon: Icons.admin_panel_settings,
                                           pressAction: () {
                                             showDialog(
                                               context: context,
                                               builder: (context) => Dialog(
-                                                child: AddNewEmployee(
-                                                  user: null,
-                                                ),
+                                                child: AdminPanelPage(),
                                               ),
                                             );
                                           },
                                         ),
                                       ),
                                     ),
-                                    if (user.title ==
-                                        DepartmentsEnum.management)
-                                      Expanded(
-                                        child: Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 5),
-                                          child: CustomButton(
-                                            title: 'Admin Panel',
-                                            leftIcon:
-                                                Icons.admin_panel_settings,
-                                            pressAction: () {
-                                              showDialog(
-                                                context: context,
-                                                builder: (context) => Dialog(
-                                                  child: AdminPanelPage(),
-                                                ),
-                                              );
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                  ],
-                                ),
+                                ],
                               ),
                             )
                           ],
@@ -111,4 +95,5 @@ class _EmployeesState extends State<Employees> {
       ),
     );
   }
+
 }
